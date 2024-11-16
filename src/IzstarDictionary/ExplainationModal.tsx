77a68b2273a86b4e8278519e9cc14091000c8aa5
Jackal_lzt <bkyarokun@gmail.com>
2024-11-16 13:42:45 +0800

    fix: dictionary modal styling

--- src/IzstarDictionary/ExplainationModal.tsx
+++ src/IzstarDictionary/ExplainationModal.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DictionaryItem } from "./index";
+import classNames from "classnames";
+import "./ExplainationModal.css";
 
 interface Props {
 	item?: DictionaryItem;
@@ -7,24 +9,45 @@ interface Props {
 }
 
 export const ExplanationModal: React.FC<Props> = ({ item, onClose }) => {
+	const [show, setShow] = useState(false);
+
+	useEffect(() => {
+		if (item) {
+			setShow(true);
+		} else {
+			setShow(false);
+		}
+	}, [item]);
+
 	if (!item) return null;
 
 	return (
-		<div className="iztro-modal-overlay">
-			<div className="iztro-modal">
-				<div className="iztro-modal-header">
-					<h3>{item.keyword}</h3>
-					<span className="iztro-modal-close" onClick={onClose}>
+		<div
+			className={classNames("iztro-modal-overlay", { show })}
+			onClick={onClose}
+		>
+			<div
+				className={classNames("iztro-modal", { show })}
+				onClick={(e) => e.stopPropagation()}
+			>
+				<div className={classNames("iztro-modal-header")}>
+					<div>{item.keyword}</div>
+					<span
+						className={classNames("iztro-modal-close")}
+						onClick={onClose}
+					>
 						×
 					</span>
 				</div>
-				<div className="iztro-modal-content">
-					<div className="iztro-modal-category">{item.category}</div>
-					<div className="iztro-modal-explanation">
+				<div className={classNames("iztro-modal-content")}>
+					<div className={classNames("iztro-modal-category")}>
+						{item.category}
+					</div>
+					<div className={classNames("iztro-modal-explanation")}>
 						{item.explanation}
 					</div>
 					{item.reference && (
-						<div className="iztro-modal-reference">
+						<div className={classNames("iztro-modal-reference")}>
 							参考：{item.reference}
 						</div>
 					)}
